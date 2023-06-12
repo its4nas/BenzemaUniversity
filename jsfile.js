ba9a5ffdc049753c0ecf0ef220f172d2b7cd249b
Anas Al-Dharei <anas202255@gmail.com>
2023-06-12 22:45:40 +0300

    first commit

---
+++ jsfile.js
@@ -0,0 +1,9 @@
+function alertfun() {
+    alert("this page is not ready yet");
+  }
+function openLoginForm(){
+  document.body.classList.add("showLoginForm");
+  }
+function closeLoginForm(){
+  document.body.classList.remove("showLoginForm");
+  }
